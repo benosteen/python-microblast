12be1173db384cf34a3d68b756e5efcd45dafa5e
bosteen <bosteen@a1a93c21-1d51-0410-bad1-e3d0453f12ee>
2008-07-03 14:55:25 +0000

    Fixed tinyurl issue with recursive tinyurls Started to flesh out twitter.py to grab friends

--- trunk/python-microblast/twitter.py
+++ trunk/python-microblast/twitter.py
@@ -1,11 +1,13 @@
 from restful_lib import Connection, ConnectionError
 from microblog_exceptions import MicroBlogMsgLimitExceeded
 
+from xml.etree import ElementTree as ET
+
 class FailWhale(Exception):
     def __str__(self):
         return "Oh noes, the failwhale cometh!"
 
-TWITTER_ENDPOINT = "http://twitter.com/statuses/update.json"
+TWITTER_ENDPOINT = "http://twitter.com/"
 
 class Twitter(object):
     def __init__(self, username,password):
@@ -22,7 +24,41 @@ class Twitter(object):
             else:
                 raise MicroBlogMsgLimitExceeded
 
-        resp = self._conn.request_post("", args={"status":message})
+        resp = self._conn.request_post("statuses/update.json", args={"status":message})
 
         if resp.get('headers').get('status') not in ["200", 200, "204", 204]:
             raise FailWhale
+
+    def listfriends(self):
+        class Friend(object):
+            def __init__(self):
+                self.name = ""
+            def __setattr__(self, name, value):
+                object.__setattr__(name,value)
+            def __str__(self):
+                return self.name
+
+        class Friends(object):
+            def __init__(self, connection):
+                self.c = connection
+                self.xml_dom = []
+                self.page=0
+            def __iter__(self):
+                if not self.xml_dom:
+                    self.get_next_batch()
+                while(len(self.xml_dom)>0):
+                    for friend in self.xml_dom.getchildren():
+                        f = Friend()
+                        for item in friend.getchildren():
+                            f.__setattr__(item.tag, item.text)
+                        yield f
+            def get_next_batch(self):
+                self.page = self.page+1
+                resp = self.c.request_get("statuses/friends.xml", args={"lite":"true", "page":self.page})
+                status = resp.get('headers').get('status')
+                if status in [200, "200"]:
+                    # TODO catch error
+                    self.xml_dom = ET.fromstring(resp.get("body").encode("UTF-8"))
+                    return
+                self.xml_dom = []
+        return Friends(self._conn) 
